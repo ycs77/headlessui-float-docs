bf2355e4586b2a647c3352908ee08f3fac66a870
Lucas Yang <yangchenshin77@gmail.com>
2022-09-13 02:20:49 +0800

    Simplify unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,32 +1,7 @@
-import {
-  defineConfig,
-  presetIcons,
-  presetUno,
-  transformerVariantGroup,
-} from 'unocss'
+import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
-  shortcuts: {
-    'border-main': 'border-gray-400 border-opacity-30',
-    'bg-main': 'bg-gray-400',
-    'bg-base': 'bg-white dark:bg-hex-1a1a1a',
-  },
   presets: [
     presetUno(),
-    presetIcons({
-      scale: 1.2,
-      warn: true,
-    }),
-  ],
-  theme: {
-    colors: {
-      primary: '#3eaf7c',
-    },
-    fontFamily: {
-      mono: 'var(--vt-font-family-mono)',
-    },
-  },
-  transformers: [
-    transformerVariantGroup(),
   ],
 })
