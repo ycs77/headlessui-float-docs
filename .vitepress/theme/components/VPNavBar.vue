af040475438a8acdd3249fd2709c3658404a58bb
Lucas Yang <yangchenshin77@gmail.com>
2024-09-01 18:07:57 +0800

    Update `<VPNavBar>` component style

--- .vitepress/theme/components/VPNavBar.vue
+++ .vitepress/theme/components/VPNavBar.vue
@@ -72,32 +72,45 @@ watchPostEffect(() => {
 <style scoped>
 .VPNavBar {
   position: relative;
-  border-bottom: 1px solid transparent;
-  padding: 0 8px 0 24px;
   height: var(--vp-nav-height);
-  transition: border-color 0.5s, background-color 0.5s;
   pointer-events: none;
+  white-space: nowrap;
+  transition: background-color 0.25s;
 }
 
-.VPNavBar.has-sidebar {
-  border-bottom-color: var(--vp-c-gutter);
+.VPNavBar.screen-open {
+  transition: none;
+  background-color: var(--vp-nav-bg-color);
+  border-bottom: 1px solid var(--vp-c-divider);
+}
+
+.VPNavBar:not(.home) {
+  background-color: var(--vp-nav-bg-color);
+}
+
+@media (min-width: 960px) {
+  .VPNavBar:not(.home) {
+    background-color: transparent;
+  }
+
+  .VPNavBar:not(.has-sidebar):not(.home.top) {
+    background-color: var(--vp-nav-bg-color);
+  }
+}
+
+.wrapper {
+  padding: 0 8px 0 24px;
 }
 
 @media (min-width: 768px) {
-  .VPNavBar {
+  .wrapper {
     padding: 0 32px;
   }
 }
 
 @media (min-width: 960px) {
-  .VPNavBar.has-sidebar {
-    border-bottom-color: transparent;
+  .VPNavBar.has-sidebar .wrapper {
     padding: 0;
-  }
-
-  .VPNavBar.fill:not(.has-sidebar) {
-    border-bottom-color: var(--vp-c-gutter);
-    background-color: var(--vp-nav-bg-color);
   }
 }
 
@@ -107,6 +120,11 @@ watchPostEffect(() => {
   margin: 0 auto;
   max-width: calc(var(--vp-layout-max-width) - 64px);
   height: var(--vp-nav-height);
+  pointer-events: none;
+}
+
+.container > .title,
+.container > .content {
   pointer-events: none;
 }
 
@@ -170,15 +188,24 @@ watchPostEffect(() => {
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  height: calc(var(--vp-nav-height) - 1px);
+  height: var(--vp-nav-height);
   transition: background-color 0.5s;
 }
 
 @media (min-width: 960px) {
-  .VPNavBar.has-sidebar .content-body,
-  .VPNavBar.fill .content-body {
+  .VPNavBar:not(.home.top) .content-body {
     position: relative;
     background-color: var(--vp-nav-bg-color);
+  }
+
+  .VPNavBar:not(.has-sidebar):not(.home.top) .content-body {
+    background-color: transparent;
+  }
+}
+
+@media (max-width: 767px) {
+  .content-body {
+    column-gap: 0.5rem;
   }
 }
 
@@ -208,27 +235,40 @@ watchPostEffect(() => {
   margin-right: -8px;
 }
 
-@media (min-width: 960px) {
-  .VPNavBar.has-sidebar .curtain {
-    position: absolute;
-    right: 0;
-    bottom: -31px;
-    width: calc(100% - var(--vp-sidebar-width));
-    height: 32px;
-  }
+.divider {
+  width: 100%;
+  height: 1px;
+}
 
-  .VPNavBar.has-sidebar .curtain::before {
-    display: block;
-    width: 100%;
-    height: 32px;
-    background: linear-gradient(var(--vp-c-bg), transparent 70%);
-    content: "";
+@media (min-width: 960px) {
+  .VPNavBar.has-sidebar .divider {
+    padding-left: var(--vp-sidebar-width);
   }
 }
 
 @media (min-width: 1440px) {
-  .VPNavBar.has-sidebar .curtain {
-    width: calc(100% - ((100vw - var(--vp-layout-max-width)) / 2 + var(--vp-sidebar-width)));
+  .VPNavBar.has-sidebar .divider {
+    padding-left: calc((100vw - var(--vp-layout-max-width)) / 2 + var(--vp-sidebar-width));
+  }
+}
+
+.divider-line {
+  width: 100%;
+  height: 1px;
+  transition: background-color 0.5s;
+}
+
+.VPNavBar:not(.home) .divider-line {
+  background-color: var(--vp-c-gutter);
+}
+
+@media (min-width: 960px) {
+  .VPNavBar:not(.home.top) .divider-line {
+    background-color: var(--vp-c-gutter);
+  }
+
+  .VPNavBar:not(.has-sidebar):not(.home.top) .divider {
+    background-color: var(--vp-c-gutter);
   }
 }
 </style>
